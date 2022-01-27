4c484ae5ead878f9f0f03cd1da113246a0d1559a
Manuel Duran <durbonca@gmail.com>
2022-01-27 19:06:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import Fountain from './script/emojiPop.tsx';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="App" onClick={() => new Fountain()}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just click wherever you like
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
